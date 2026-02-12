5b9dcafb50dd6b4a581deca13c7f62c76f2e930e
FerneyLopez <atecnologia@FBOLIVAR.LOCAL>
2026-02-12 10:10:59 -0500

    Fix: Update platavirtual view we added the Estado Op field .

--- backend/app/ipsl/models.py
+++ backend/app/ipsl/models.py
@@ -6,9 +6,11 @@ class OrdenProduccionForja(models.Model):
     codigo_herramental = models.CharField(null=True, blank=True,max_length=1024, db_column='Codigo Herramental')
     numero_pedido = models.CharField(max_length=20, db_column='Numero de Pedido')
     consecutivo_op = models.CharField(max_length=20, primary_key=True, db_column='Orden de Produccion')
+    estado_op = models.CharField(max_length=20, db_column='Estado OP')
     fecha_inicio = models.DateTimeField(db_column='Fecha de Inicio')
     comentario = models.TextField(null=True, blank=True, db_column='Comentario')
     producto = models.TextField(null = True, max_length=50, db_column='Producto')
+
 
     class Meta:
         db_table = 'v_plantaVirtual'
